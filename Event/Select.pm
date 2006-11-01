73432038894588b24927d23d7a74ae40360af63d
Marc A. Lehmann <schmorp@schmorp.de>
2006-11-01 21:39:03 +0000

    *** empty log message ***

--- Event/Select.pm
+++ Event/Select.pm
@@ -49,11 +49,11 @@ sub import {
 
 sub select(;*$$$) { # not the correct prototype, but well... :()
    if (@_ == 0) {
-      return CORE::select;
+      return CORE::select
    } elsif (@_ == 1) {
-      return CORE::select $_[0];
+      return CORE::select $_[0]
    } elsif (defined $_[3] && !$_[3]) {
-      return &CORE::select (@_);
+      return CORE::select $_[0], $_[1], $_[2], $_[3]
    } else {
       my $current = $Coro::current;
       my $nfound = 0;
@@ -85,7 +85,7 @@ sub select(;*$$$) { # not the correct prototype, but well... :()
       # wait here
 
       $_->cancel for @w;
-      return $nfound;
+      return $nfound
    }
 }
 
