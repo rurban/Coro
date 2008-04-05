feb3f7bb39520f34da359282675d785f0fad9b8d
Marc A. Lehmann <schmorp@schmorp.de>
2008-04-05 22:38:26 +0000

    *** empty log message ***

--- Coro/State.pm
+++ Coro/State.pm
@@ -264,10 +264,16 @@ coroutines. Currently, Coro will limit the number of idle/unused cctxs to
 Returns the current C stack size and optionally sets the new I<minimum>
 stack size to C<$new_stacksize> I<long>s. Existing stacks will not
 be changed, but Coro will try to replace smaller stacks as soon as
-possible. Any Coro::State's that starts to use a stack after this call is
-guarenteed this minimum size. Please note that Coroutines will only need
-to use a C-level stack if the interpreter recurses or calls a function in
-a module that calls back into the interpreter.
+possible. Any Coro::State that starts to use a stack after this call is
+guaranteed this minimum stack size. Please note that Coroutines will
+only need to use a C-level stack if the interpreter recurses or calls a
+function in a module that calls back into the interpreter.
+
+=item Coro::State::force_cctx
+
+Forces the allocation of a C context for the currently running coroutine
+(if not already done). Apart from benchmarking there is little point
+in doing so, however.
 
 =item @states = Coro::State::list
 
