752b8fe94afe95777d07aff320f969e3b6dbc937
Marc A. Lehmann <schmorp@schmorp.de>
2008-11-08 16:46:32 +0000

    4.9

--- Coro/State.pm
+++ Coro/State.pm
@@ -92,7 +92,7 @@ sub warnhook { &$WARNHOOK }
 use XSLoader;
 
 BEGIN {
-   our $VERSION = 4.804;
+   our $VERSION = 4.9;
 
    # must be done here because the xs part expects it to exist
    # it might exist already because Coro::Specific created it.
