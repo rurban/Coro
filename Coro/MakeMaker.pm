6f2880ec966ee85dc9d23e227a571249230379ec
Marc A. Lehmann <schmorp@schmorp.de>
2009-07-06 03:42:29 +0000

    5.151

--- Coro/MakeMaker.pm
+++ Coro/MakeMaker.pm
@@ -5,7 +5,7 @@ BEGIN { eval { require warnings } && warnings->unimport ("uninitialized") }
 use Config;
 use base 'Exporter';
 
-our $VERSION = 5.15;
+our $VERSION = 5.151;
 
 @EXPORT_OK = qw(&coro_args $installsitearch);
 
