bb94d7badfe1ad658b82bbc0fc9dd6e413300d68
Marc A. Lehmann <schmorp@schmorp.de>
2010-03-07 16:56:16 +0000

    *** empty log message ***

--- myhttpd/misc.pl
+++ myhttpd/misc.pl
@@ -1,7 +1,13 @@
 
 sub escape_uri {
    local $_ = shift;
-   s/([()<>%&?,; ='"\x00-\x1f\x80-\xff])/sprintf "%%%02X", ord($1)/ge;
+   s/([;\/?:@&=+\$,()<>% '"\x00-\x1f\x7f-\xff])/sprintf "%%%02X", ord($1)/ge;
+   $_;
+}
+
+sub escape_attr {
+   local $_ = shift;
+   s/(['<>&\x00-\x1f\x80-\x9f])/sprintf "&#%d;", ord($1)/ge;
    $_;
 }
 
